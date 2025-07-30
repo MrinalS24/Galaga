db292a00c2872be29aa4165bfc2c3da534398e52
MrinalS24 <connectwithmrinaal@gmail.com>
2025-07-30 14:59:28 +0530

    main.py

--- Galaga/main.py
+++ Galaga/main.py
@@ -5,29 +5,59 @@ WIDTH = 1000
 HEIGHT = 700
 
 aliens = []
+tanks = []
+motherships = []
 level = 1
-end_game = False
+message = ""
+is_winning = None
 
 
 bullets =[]
 
 fighter = Actor("fighter")
-mothership = Actor("mothership")
 
-#bullets = [bullet_1, bullet_2, bullet_3, bullet_4, bullet_5, bullet_6, bullet_7, bullet_8, bullet_9, bullet_10]
+
 
 fighter.pos = WIDTH/2, HEIGHT - 80
 
 
+def game_over():
+    global message
+    if is_winning == False:
+        message = "Too bad. You Lost..."
+    else:
+        message = "Great Job! You Won!"
+        sounds.winning.play()
+
+
+
 def draw():
-    screen.clear()
-    screen.blit("sky", (0,0))
-    for b in bullets:
-        b.draw()
-    fighter.draw()
-    for i in aliens:
-        i.draw()
-    screen.draw.text(str(level), (800, 0), color = "white")
+    global is_winning, count
+    if level < 6:
+        screen.clear()
+        screen.blit("sky", (0,0))
+        for b in bullets:
+            b.draw()
+        fighter.draw()
+        for i in aliens:
+            i.draw()
+        for t in tanks:
+            t.draw()
+        if level == 5:
+            for i in motherships:
+                i.draw()
+            if count == 5:
+                motherships.remove(m)
+                sounds.explosion.play()
+        screen.draw.text(f"You are on level: {level}", (800, 50), color = "white")
+
+
+    else:
+        screen.fill(color = "black")
+        screen.draw.text(message, (WIDTH/2, HEIGHT/2), color = "Yellow")
+        is_winning = True
+        game_over()
+
     
 
 
@@ -45,8 +75,7 @@ def create_alien():
         aliens.append(soldier_1)
         aliens.append(soldier_2)
         aliens.append(soldier_3)
-    
-    elif level > 1:
+    else:
         soldier_1 = Actor("green")
         soldier_2 = Actor("green")
         soldier_3 = Actor("green")
@@ -70,11 +99,58 @@ def create_alien():
         aliens.append(soldier_4)
         aliens.append(soldier_5)
         aliens.append(soldier_6)
+    if level > 2:
+        tank_1 = Actor("purple")
+        tank_2 = Actor("purple")
+        tank_3 = Actor("purple")
+        tx_value = random.randint(50, 300)
+        ty_value = -200
+        tank_1.pos = tx_value, ty_value
+        tank_2.pos = tx_value + 125, ty_value + 75
+        tank_3.pos = tx_value + 250, ty_value
+        tanks.append(tank_1)
+        tanks.append(tank_2)
+        tanks.append(tank_3)
+    if level > 3:
+        tank_4 = Actor("purple")
+        tank_5 = Actor("purple")
+        tank_6 = Actor("purple")
+        tx_value = random.randint(500, 800)
+        ty_value = -200
+        tank_4.pos = tx_value, ty_value
+        tank_5.pos = tx_value + 125, ty_value + 75
+        tank_6.pos = tx_value + 250, ty_value
+        tanks.append(tank_4)
+        tanks.append(tank_5)
+        tanks.append(tank_6)
+    if level > 4:
+        tank_7 = Actor("purple")
+        tank_8 = Actor("purple")
+        tank_9 = Actor("purple")
+        tx_value = random.randint(200, 800)
+        ty_value = -400
+        tank_7.pos = tx_value, ty_value
+        tank_8.pos = tx_value + 125, ty_value + 75
+        tank_9.pos = tx_value + 250, ty_value
+        tanks.append(tank_7)
+        tanks.append(tank_8)
+        tanks.append(tank_9)
+
+        mothership = Actor("mothership")
+        mx = 500
+        my = -600
+        mothership.pos = mx, my
+        motherships.append(mothership)
+
+
+
+
+
 
 
 
 def update():
-    global bullets, fighter, level
+    global bullets, fighter, level, is_winning, count
     if aliens:
         for a in aliens:
             a.y += 1
@@ -87,15 +163,65 @@ def update():
             j.y -= 4
             if j.y <=0:
                 bullets.remove(j)
-                print(bullets)
             for g in aliens:
                 if j.colliderect(g):
                     aliens.remove(g)
                     bullets.remove(j)
+                    #sounds.explosion.play()
+    elif tanks:
+        for t in tanks:
+            t.y += 1
+        if keyboard.d:
+            fighter.x += 4
+        elif keyboard.a:
+            fighter.x -= 4
+
+        for j in bullets:
+            j.y -= 4
+            for s in tanks:
+                if j.colliderect(s):
+                    tanks.remove(s)
+                    bullets.remove(j)
                     sounds.explosion.play()
+            if j.y <=0:
+                bullets.remove(j)
+    elif motherships:
+        for m in motherships:
+            m.y += 1
+            if keyboard.d:
+                fighter.x += 4
+            elif keyboard.a:
+                fighter.x -= 4
+            count = 0
+            for l in bullets:
+                l.y -= 4
+                if l.y <= 0:
+                    bullets.remove(l)
+                if l.colliderect(m):
+                    count += 1
+                    bullets.remove(l)
+
+
+
     else:
         level += 1
         create_alien()
+    
+    for a in aliens:
+        if a.y >= WIDTH or a.colliderect(fighter):
+            is_winning = False
+            game_over()
+    for t in tanks:
+        if t.y >= WIDTH or t.colliderect(fighter):
+            is_winning = False
+            game_over()
+    for m in motherships:
+        if m.y >= WIDTH or m.colliderect(fighter):
+            is_winning = False
+            game_over()
+        
+    
+
     
 
 
@@ -106,7 +232,7 @@ def on_mouse_down():
     bullet = Actor("bullet")
     bullets.append(bullet)
     bullet.pos = fighter.pos
-    sounds.gunshot.play()
+    #sounds.gunshot.play()
 
 
     
